3c90b91be81cb65b62886a21fef3569add957e5d
liu-chien <liu_chien@gapp.nthu.edu.tw>
2019-10-21 22:03:54 +0800

    agent version

--- book_field.py
+++ book_field.py
@@ -1,14 +1,5 @@
 import requests
-
-def check_response(response):
-    try:
-        response.raise_for_status()
-    except HTTPError as http_err:
-        print('HTTP error occurred: {}'.format(http_err))  # Python 3.6
-    except Exception as err:
-        print('Other error occurred: {}'.format(err))  # Python 3.6
-    else:
-        print('Success!')
+from requests.exceptions import HTTPError
 
 def check_result(text, key1='<script>alert', key2='</script></head>'):
     text = text[1100:1300]
@@ -43,6 +34,52 @@ def book_field(field_number, time_slot, date, account, passwd):
         r.encoding = 'utf-8'
 
         return check_result(r.text)
+
+class Agent:
+    def __init__(self, time_slot, date, account, passwd):
+        self.usr = {'account': account,'password': passwd}
+        self.date = {'field': None, 'timeSlot': str(time_slot), 'account': account, 'date': str(date)}
+        self.sess = requests.Session()
+        self.log_in()
+
+    def log_in(self):
+        print('Log in...')
+        p = self.sess.post('http://peo.nthu.edu.tw/nthugym/reservation/login_successfully.php', data=self.usr)
+        self.check_response(p)
+
+    def book_field(self, field_number):
+        self.date['field'] = str(field_number)
+        r = self.sess.post('http://peo.nthu.edu.tw/nthugym/reservation/reservAction.php', data=self.date)
+        r.encoding = 'utf-8'
+        return self.check_result(r.text)
+        
+    def check_response(self, response):
+        try:
+            response.raise_for_status()
+        except HTTPError as http_err:
+            print('HTTP error occurred: {}'.format(http_err))  # Python 3.6
+        except Exception as err:
+            print('Other error occurred: {}'.format(err))  # Python 3.6
+        else:
+            print('Success!')
+    
+    def check_result(self, text, key1='<script>alert', key2='</script></head>'):
+        text = text[1100:1300]
+        start = text.find(key1)
+        end = text.find(key2)
+        result = text[start+len(key1):end]
+        if len(result) == 15:       # ('已經有人預約或尚未開放') or ('同一時段只能預借兩個場')
+            print(result)
+            return False
+        elif len(result) == 40:     # ("預約成功!提醒您~ 進入球場前請在大廳換上室內運動鞋，且禁穿黑色膠底鞋。")
+            print(result)
+            return True
+        else:
+            print(result)
+            return None
+
+    
+        
 
 if __name__ == '__main__':
     from account import account
